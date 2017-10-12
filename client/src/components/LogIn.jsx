2f7bc2a7181c46e16e4b0b45e28a7166db6ce34a
supertopoz <deepsleepsounds@gmail.com>
2017-10-13 07:41:35 +0900

    add again login and sign up pages

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  FormControl,
+  Button,
+  FormGroup,
+  Form,
+  Panel,
+  Grid,
+  Row,
+  Col,
+
+} from "react-bootstrap";
 
 
 var Login = (props) => (
-  <div className="loginPage">
-    <h1>Log In</h1>
-    <form onSubmit={props.login}>
-      <label htmlFor="username">Username:</label>
-      <input id="username" type="text" name="username"/>
-      <label htmlFor="password">Password:</label>
-      <input id="password" type="password" name="password"/>
-      <input type="submit" value="Log In"/>
-    </form>
+
+  <div className="loginPage" onClick={()=> props.hideAlert() }>
+    <Grid>
+      <Row>
+        <Col md={6} mdOffset={3}>
+          <Panel className="login-panel">
+            <h1>Log In</h1>
+            <div className={props.alert}>Warning: {props.alertText}</div>
+            <Form horizontal onSubmit={props.login}>
+              <FormGroup>
+                <Col md={6} mdOffset={3}>
+                  <FormControl
+                    id="username"
+                    type="email"
+                    placeholder="Email"
+                    name="username"
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Col md={6} mdOffset={3}>
+                  <FormControl
+                    d="password"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Col md={12} sm={12}>
+                  <Button bsStyle="primary" type="submit">Log in</Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Panel>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 );
-
-
-export default Login;
