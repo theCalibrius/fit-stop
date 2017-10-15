02f7702647eca9d3dc6a2f0e5fe9f3a3f37bf4ea
ArtemisD <dianah456@gmail.com>
2017-10-14 20:42:59 -0400

    more gifs

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -491,7 +491,7 @@ var cobra = {
   name: 'cobra stretch',
   description: 'Lie face down with your hands under your shoulders.Point your feet downwards to lengthen your spine.Slowly push your torso up as far as you comfortably can – try to get your hips to rise off the floor slightly.Hold the stretch and then lower down to starting position.',
   type: 'cooldown',
-  picture: 'https://d24bnpykhxwj9p.cloudfront.net/s3file/s3fs-public/users1/2017-07/Tue/cobra-gif.gif',
+  picture: 'https://static1.squarespace.com/static/56ee1fcbc6fc08c51ceae771/57434ec41bbee07d9fb43342/57434ed801dbae831f56c591/1464028892736/Cobra+Stretch_500p.gif',
   environment: 'indoor',
   muscleGroup: 'core',
   difficulty: 'easy'
