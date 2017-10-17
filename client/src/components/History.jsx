d5cfed51bf5cd57aed5ab81d08f3c8ed93a160d1
supertopoz <deepsleepsounds@gmail.com>
2017-10-18 08:55:31 +0900

    fixed date format

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PastWorkout from './PastWorkout.jsx'
-
+import moment from 'moment'
 
 
 
 var History = (props) => (
   <div className="container-fluid history text-center">
     <h2> History </h2>
-    {props.loggedIn && props.workoutHistory.map(indivWorkout => <PastWorkout date={indivWorkout.date} lengthOfWorkout={indivWorkout.lengthOfWorkout} key={indivWorkout._id}/>)}
+    {props.loggedIn && props.workoutHistory.map(indivWorkout => <PastWorkout date={moment(indivWorkout.date).format("ddd YYYY-MM-DD HH:MM")} lengthOfWorkout={indivWorkout.lengthOfWorkout} key={indivWorkout._id}/>)}
     {!props.loggedIn && (<span className='historyNotLoggedIn'>You are not currently logged in. Please Log In or Sign Up to view Workout History.</span>)}
   </div>
 );
